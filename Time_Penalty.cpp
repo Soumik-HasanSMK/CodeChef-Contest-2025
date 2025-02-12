2a737e03c0addf9870b97f1e4cb2a01ea1dfad9e
Soumik Hasan <soumik.hasan.smk@gmail.com>
2025-02-12 21:34:20 +0600

    Starters 173

---
+++ Time_Penalty.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define db double
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + (b * 10) << endl;
+    return 0;
+}
